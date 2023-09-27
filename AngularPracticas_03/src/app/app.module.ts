b492bc9c1953882cadcc49cf536d1e652c0ad6c8
Fernando Guzman <fernandoguzman623@gmail.com>
2023-09-27 17:07:22 -0600

    ---AngularPracticas_03 FDGB/ 27/Septiembre/2023

---
+++ AngularPracticas_03/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { OperasBasComponent } from './calculos/operas-bas/operas-bas.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OperasBasComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+  
+ }
